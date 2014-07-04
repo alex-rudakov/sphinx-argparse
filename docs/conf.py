ea538aa40c528cb5489393279f4d09854e482e4c
Alex Rudakov <ribozz@gmail.com>
2014-07-04 22:07:38 +0300

    Fix failing readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or extra_modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import sphinx_rtd_theme
+
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -29,8 +29,13 @@ sys.path.insert(0, os.path.abspath('..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinxarg.ext']
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Add any paths that contain templates here, relative to this directory.
